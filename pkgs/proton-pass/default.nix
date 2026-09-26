a3c4105eace3194b183b84c5f310c9e5561b3df3
Nanamiiiii <52265804+Nanamiiiii@users.noreply.github.com>
2026-09-26 07:11:02 +0000

    Bump package version

--- pkgs/proton-pass/default.nix
+++ pkgs/proton-pass/default.nix
@@ -10,17 +10,17 @@
 }:
 let
   pname = "proton-pass";
-  version = "1.40.2";
+  version = "1.41.1";
 
   passthru = {
     sources = {
       "x86_64-linux" = fetchurl {
         url = "https://proton.me/download/pass/linux/x64/proton-pass_${version}_amd64.deb";
-        hash = "sha256-NHpQ1eftyKrysqQ41XGfAbHEUZ7hjarTyOIBpsF2Vho=";
+        hash = "sha256-vQLZtWAitEgADCsL43cafWYkZy+uvDO3FGyxd3XmXog=";
       };
       "aarch64-darwin" = fetchurl {
         url = "https://proton.me/download/pass/macos/ProtonPass_${version}.dmg";
-        hash = "sha256-+33eXc988leX22ByXGST9w9vsUcrD+PP8eUaoHtWDKI=";
+        hash = "sha256-lUUUj6wbWdo3d/wYRZ8DtwxAYMqM4oJC3cpfNbF/g6M=";
       };
       "x86_64-darwin" = passthru.sources."aarch64-darwin";
     };
